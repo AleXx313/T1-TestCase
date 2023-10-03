a303d929fc66a671fbda34abb8fc16594b0f4138
Alexey Mironov <alexeymironov1991@gmail.com>
2023-10-03 20:10:39 +0300

    feat: Реализовать основной функционал тестового задания

---
+++ src/main/java/com/example/t1_testcase/T1TestcaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.t1_testcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T1TestcaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(T1TestcaseApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/t1_testcase/util/PatternConstants.java
@@ -0,0 +1,7 @@
+package com.example.t1_testcase.util;
+
+public class PatternConstants {
+
+    public static final String LATIN_AND_KIRILL_PATTERN = "^[а-яА-ЯёЁa-zA-Z0-9]+$";
+    public static final String LATIN_PATTERN = "^[a-zA-Z0-9]+$";
+}
